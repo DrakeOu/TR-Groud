5e72aab92ea14e3b69e5935e62837ddb0d7202ca
DrakeOu <279535610@qq.com>
2020-08-23 11:32:36 +0800

    备份code
    Signed-off-by: DrakeOu <279535610@qq.com>

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import vuetify from "./plugins/vuetify";
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+import Chat from 'jwchat'
+Vue.use(Chat)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  vuetify,
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
